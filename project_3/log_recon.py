57b02b340621b3e93dbd72327d29041a00ccbd7e
Muhammad Rudyn <nydurism@gmail.com>
2026-04-28 17:59:43 +0800

    Initial update for python script

---
+++ project_3/log_recon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Preliminary step: Define modules
+import re # To work with regular expressions (regex)
+import os # To use os functionality
+from collections import Counter # To count hashable objects
+from datetime import datetime # To store info a date/time objects
+
+# Step 1: Parse log file(s)
+
+
+
+# Step 2: Analyse log file(s)
+
+# Refer to devtest/auth_log_analyse.sh as base guideline of info to be extracted
+
+
+# Step 3: Compile result(s) for report
