99ade1f1408d9350d642c07306d632e4df5cd744
Muhammad Rudyn <nydurism@gmail.com>
2026-04-29 10:21:02 +0800

    parse_test.py update

--- project_3/devtest/parse_test.py
+++ project_3/devtest/parse_test.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
-# Converting bash to python test
+# Converting from the sample bash script (auth_log_analyse.sh) to python script
 
 import re
 import os
 from collections import Counter
 
-log_file = 'sample_auth.log'
-
-with open(log_file, 'r') as f:
-    for line in f:
-        print(line.strip())
+def parse_auth_log(filename='sample_auth.log'): # Parsing from a single log file
+    """Parse authentication log file and extract relevant information"""
+    # For 'failed' results
+    failed_attempts=[]
+    failed_root=[]
+    failed_invalid=[]
+    failed_valid=[]
+    # For IP
+    failed_ips=[]
+    # For username
+    invalid_usernames=[]
+    valid_usernames=[]
+    # For 'successful' results
+    success_root=[]
+    success_root_ips=[]
+    
