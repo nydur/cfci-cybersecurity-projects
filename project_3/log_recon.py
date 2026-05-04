627c77a89fb68e9bd156eda0c2d5c5cfbdb91e10
Muhammad Rudyn <nydurism@gmail.com>
2026-05-04 11:56:05 +0800

    Update to log_recon

--- project_3/log_recon.py
+++ project_3/log_recon.py
@@ -3,16 +3,20 @@
 # Preliminary step: Define modules
 import re # To work with regular expressions (regex)
 import os # To use os functionality
-from collections import Counter # To count hashable objects
 from datetime import datetime # To store info a date/time objects
 
 # Step 1: Parse log file(s)
+def parse_log(filepath):
 
 
-
-# Step 2: Analyse log file(s)
-
-# Refer to devtest/auth_log_analyse.sh as base guideline of info to be extracted
+# Step 2: Analyse result(s) of log file(s)
+def analyse_events(events):
 
 
 # Step 3: Compile result(s) for report
+def save_report(findings, output_file, log_files):
+
+
+# Main step: Runs program
+if __name__ == "__main__":
+    
